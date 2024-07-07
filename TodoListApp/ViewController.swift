e697a05c498ddc8fe9dce6fabc209e46faecdbc3
abdul'Karim <iCodeCreator>
2024-07-07 17:24:52 +0300

    Initial Commit

---
+++ TodoListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoListApp
+//
+//  Created by abd ul’Karim 📚 on 07.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
